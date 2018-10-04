b7ec74e4cdb018cae18ca8f085a3993dd564534a
Katrina Tran <kat.c.tran@gmail.com>
2018-10-03 18:48:34 -0600

    added todo

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 class TodoForm extends React.Component {
+    state = {name: ''}
+
+    handleChange = (e) => {
+     this.setState({ name: e.target.value });
+   }
+
+   handleSubmit = (e) => {
+     e.preventDefault();
+     console.log(this.state.name)
+   }
   render() {
+    const {name} = this.state;
     return (
-      <form>
-        <input required placeholder="What to do" />
+      <form onSubmit={this.handleSubmit}>
+
+        <input
+         value={name}
+         name="name"
+         onChange={this.handleChange}
+           required
+           placeholder="What to do" />
       </form>
     )
   }
